706fb4462d809cc894be135251c32f5968688155
paulycap <105924763+xypaulycap@users.noreply.github.com>
2024-06-21 19:24:58 -0700

    updated backend url in storeContext

--- food del frontend/src/Context/StoreContext.jsx
+++ food del frontend/src/Context/StoreContext.jsx
@@ -6,8 +6,8 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
+  const url ="https://food-del-backend-w1bn.onrender.com"
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000"
   const [token, setToken] = useState("")
   const [food_list, setFoodList] =useState([])
 
